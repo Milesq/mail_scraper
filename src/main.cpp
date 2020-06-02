595bc3118525716dbf4fe65e89c2697f6ceda2bc
Milesq <wmilosz88@gmail.com>
2020-06-02 13:57:51 +0200

    Add success message box

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <string>
 #include <fstream>
 #include <vector>
+#include <windows.h>
 
 #include <inipp.h>
 
@@ -11,6 +12,8 @@
 using namespace std;
 using inipp::Ini;
 
+void success(uint32_t = 0);
+
 int main(int argc, const char **argv)
 {
     try
@@ -18,6 +21,7 @@ int main(int argc, const char **argv)
         if (argc == 1)
         {
             config([]() {
+                success();
                 throw CustomError::CONFIG_SERVER_CLOSE;
             });
 
@@ -53,13 +57,20 @@ int main(int argc, const char **argv)
         if (fs::is_regular_file(dir))
         {
             execute_add_record(read_file(dir).c_str());
+            success(1);
         }
         else if (fs::is_directory(dir))
+        {
+            uint32_t filesCount = 0;
             for (auto &p : fs::directory_iterator(dir))
             {
+                ++filesCount;
                 string file = read_file(p.path().string());
                 execute_add_record(file.c_str());
             }
+
+            success(filesCount);
+        }
         else
             assert(false, L"Musisz podać folder albo plik");
     }
@@ -72,4 +83,23 @@ int main(int argc, const char **argv)
 #endif
 
     return 0;
+}
+
+void success(uint32_t filesCount)
+{
+    wstring w_msg;
+    wstringstream wss;
+    wss << L"Program zakończył pracę.\n";
+
+    if (filesCount != 0)
+    {
+        wss << "Przeskanowano "
+            << filesCount
+            << L" plik/i/ów";
+    }
+
+    w_msg.append(wss.str());
+
+    const wchar_t *msg = w_msg.c_str();
+    MessageBoxW(NULL, msg, L"Info", MB_OK);
 }
