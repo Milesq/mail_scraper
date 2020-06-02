4fcdf31ddb0958b3da8878e87ef126094e9cb75b
Milesq <wmilosz88@gmail.com>
2020-06-02 16:53:08 +0200

    Update web_server to git repo

--- src/config_server.rs
+++ src/config_server.rs
@@ -23,23 +23,11 @@ pub extern "C" fn config(on_close: extern "C" fn()) {
                 "asd".into()
             }),
         )
-        .not_found(Box::new(|req: Request, mut default_resp: Response| {
-            let mut path = env::current_exe()
-                .expect("Cannot find current exe")
-                .parent()
-                .unwrap()
-                .to_path_buf();
-
+        .not_found(Box::new(|req: Request, _| {
+            let mut path = exe_dir!();
             path.push(format!("./static{}", req.get_path()));
 
-            if path.exists() {
-                path.as_path().into()
-            } else {
-                default_resp
-                    .set_http_code(HttpCode::_404)
-                    .set_body("Nie znaleziono pliku");
-                default_resp
-            }
+            path.as_path().into()
         }))
         .launch(crate::CONFIG_SERVER_PORT);
 }
