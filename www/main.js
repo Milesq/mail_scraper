cb0a8893c224bbfeee07197eb0a71a0a4d35ee35
Milesq <wmilosz88@gmail.com>
2020-06-09 16:51:33 +0200

    Restore www functionality

--- www/main.js
+++ www/main.js
@@ -11,37 +11,21 @@ const chunks = (arr, chunkSize) =>
     .fill()
     .map((_, i) => arr.slice(i * chunkSize, i * chunkSize + chunkSize));
 
+/**
+ * @param {string} str
+ * @returns {Object}
+ */
 const parse = str => Object.fromEntries(chunks(wasm.parse_mail(str), 2));
 
-const txt = `
-imie:
-asdd
+const fieldsInput = document.getElementById('fields');
+const closeBtn = document.getElementById('close-button');
 
-nazwisko:
-ads
+document.forms.mainform.addEventListener('submit', c => {
+  fieldsInput.value = fieldsInput.value
+    .split(',')
+    .map(el => el.trim())
+    .join(',')
+    .slice(0, -1); // Remove last char
+});
 
-dsa
-dsa
-dasd
-as
-
-
-das
-d
-
-`;
-
-console.log(parse(txt));
-
-// const fieldsInput = document.getElementById('fields');
-// const closeBtn = document.getElementById('close-button');
-
-// document.forms.mainform.addEventListener('submit', c => {
-//   fieldsInput.value = fieldsInput.value
-//     .split(',')
-//     .map(el => el.trim())
-//     .join(',')
-//     .slice(0, -1); // Remove last char
-// });
-
-// closeBtn.addEventListener('click', () => fetch('/close'));
+closeBtn.addEventListener('click', () => fetch('/close'));
